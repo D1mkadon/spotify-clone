a29bbc0c5a9d5daa76bbecbb133f26f982580c04
D1mkadon <dima0938157970@gmail.com>
2024-02-20 22:51:12 +0100

    changes

---
+++ src/app/data/cardsData.js
@@ -0,0 +1,37 @@
+export const cardsData = [
+  {
+    title: "Create your first playlist",
+    description: "It's easy, we'll help you",
+    img: "https://via.placeholder.com/300x200.png?text=Card+1",
+  },
+  {
+    title: "Let's find some podcasts to follow",
+    description: "We'll keep you updated on new episodes",
+    img: "https://via.placeholder.com/300x200.png?text=Card+1",
+  },
+  {
+    title: "Let's find some podcasts to follow",
+    description: "We'll keep you updated on new episodes",
+    img: "https://via.placeholder.com/300x200.png?text=Card+1",
+  },
+  {
+    title: "Let's find some podcasts to follow",
+    description: "We'll keep you updated on new episodes",
+    img: "https://via.placeholder.com/300x200.png?text=Card+1",
+  },
+  {
+    title: "Let's find some podcasts to follow",
+    description: "We'll keep you updated on new episodes",
+    img: "https://via.placeholder.com/300x200.png?text=Card+1",
+  },
+  {
+    title: "Let's find some podcasts to follow",
+    description: "We'll keep you updated on new episodes",
+    img: "https://via.placeholder.com/300x200.png?text=Card+1",
+  },
+  {
+    title: "Let's find some podcasts to follow",
+    description: "We'll keep you updated on new episodes",
+    img: "https://via.placeholder.com/300x200.png?text=Card+1",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import LeftBlock from "./components/LeftBlock";
 import BottomBlock from "./components/BottomBlock";
+import Image from "next/image";
+import { cardsData } from "./data/cardsData";
 const Home = () => {
   return (
     <div className="block">
@@ -23,16 +25,43 @@ const Home = () => {
         <div className="bgMain"></div>
         <div className="h-16"></div>
         <main className="z-[1]">
-          <div>
-            <div className="px-6">
+          <section>
+            <section className="px-6">
               <div className="flex justify-between ">
                 <a href="/">Spotify Playlist</a>
                 <a>show all</a>
               </div>
-            </div>
-            <div>Sleep</div>
-          </div>
-          <div></div>
+              <div className="musicSection ">
+                {cardsData.map((e, index) => (
+                  <div
+                    key={index}
+                    className="p-4 rounded-lg relative bg-[#181818]"
+                  >
+                    <div className="pb-[100%] relative mb-4">
+                      <Image
+                        src={e.img}
+                        alt="/"
+                        fill
+                        className="w-full  h-full top-0 left-0 object-cover object-center"
+                      />
+                    </div>
+                    <div className="max-w-full min-h-[62px]">
+                      <p className="inline-block max-w-full pb-1 text-base font-bold overflow-hidden text-ellipsis whitespace-nowrap h-[26px]">
+                        {e.title}
+                      </p>
+                      <span className="text-[#a7a7a7] text-[0.875rem]">
+                        {e.description}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </section>
+            <section>
+              <div>Sleep</div>
+            </section>
+          </section>
+          <section>footer</section>
         </main>
       </div>
       <BottomBlock />
