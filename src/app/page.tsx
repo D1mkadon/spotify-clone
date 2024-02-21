3ed3b438f82df0ff8e838c5486823426a024121c
D1mkadon <dima0938157970@gmail.com>
2024-02-21 19:24:05 +0100

    couple changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,14 +29,14 @@ const Home = () => {
         </header>
         <div className="bgMain"></div>
         <div className="h-16"></div>
-        <main className="z-[1] pt-2">
-          <section className="MusicContainer">
-            <div className="flex justify-between items-center w-full px-4 h-[30px]">
-              <a href="/" className="hover:underline font-extrabold">
-                Spotify Playlist
+        <main className="z-[1] ">
+          <section className="MusicContainer font-bold">
+            <div className="flex justify-between items-center w-full  h-[30px]">
+              <a href="/" className="hover:underline text-[22px] pl-0">
+                Spotify Playlists
               </a>
               <a href="/" className="hover:underline text-[#B3b3b3]">
-                show all
+                <span className="ml-2 mt-[2px] text-[13px]"> Show all</span>
               </a>
             </div>
             <section className="">
@@ -44,7 +44,7 @@ const Home = () => {
                 {cardsData.map((e, index) => (
                   <div
                     key={index}
-                    className="p-4 rounded-lg relative bg-[#181818] cursor-pointer hover:bg-[#282828] transition-all duration-[0.3] ease-in-out"
+                    className="p-4 rounded-lg relative bg-[#181818] cursor-pointer hover:bg-[#282828] h-full w-full transition-all duration-[0.3] ease-in-out"
                   >
                     <div className="pb-[100%] relative mb-4">
                       <Image
@@ -58,19 +58,19 @@ const Home = () => {
                       <p className="inline-block max-w-full pb-1 text-base font-bold overflow-hidden text-ellipsis whitespace-nowrap h-[26px]">
                         {e.title}
                       </p>
-                      <span className="text-[#a7a7a7] text-[0.875rem]">
+                      <div className="MusicCardDescription">
                         {e.description}
-                      </span>
+                      </div>
                     </div>
                   </div>
                 ))}
               </div>
             </section>
-            <section>
+            {/* <section>
               <div>Sleep</div>
-            </section>
+            </section> */}
           </section>
-          <section className="pb-[40px] px-8 pt-2 w-full box-border">
+          <section className="pb-[40px] px-8 pt-12 w-full box-border">
             <div className="mt-8 flex justify-between flex-row">
               <div className="flex-[1_1_50%] flex flex-row text-[15px]">
                 {RightFooterLinks.map((e, index) => (
@@ -87,13 +87,52 @@ const Home = () => {
                   </ul>
                 ))}
               </div>
-              <div>
-                <a href="/">Inst</a>
-                <a href="/">Inst</a>
-                <a href="/">Inst</a>
+              <div className="mb-10">
+                <div className="pr-4 inline">
+                  <a
+                    href="/"
+                    className="hover:bg-[#727272] h-10 w-10 rounded-full bg-[#292929] inline-flex justify-center items-center"
+                  >
+                    <Image
+                      src={"instagram.svg"}
+                      alt="spotify Logo"
+                      width={16}
+                      height={16}
+                    />
+                  </a>
+                </div>
+                <div className="pr-4 inline">
+                  <a
+                    href="/"
+                    className="h-10 w-10 rounded-full hover:bg-[#727272] bg-[#292929] inline-flex justify-center items-center"
+                  >
+                    <Image
+                      src={"twitter.svg"}
+                      alt="spotify Logo"
+                      width={16}
+                      height={16}
+                    />
+                  </a>
+                </div>
+                <div className="pr-4 inline">
+                  <a
+                    href="/"
+                    className="h-10 w-10 rounded-full bg-[#292929] hover:bg-[#727272] inline-flex justify-center items-center"
+                  >
+                    <Image
+                      src={"facebook.svg"}
+                      alt="spotify Logo"
+                      width={16}
+                      height={16}
+                    />
+                  </a>
+                </div>
               </div>
             </div>
-            footer
+            <hr className="border-t border-white/[0.1] mb-6" />
+            <div className="flex flex-row items-start pt-4 text-[14px] text-[#A7A7A7]">
+              © 2024 Spotify AB
+            </div>
           </section>
         </main>
       </div>
