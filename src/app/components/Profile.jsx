7240b9c3e7a186940989dc12ffc0351b255f8fd9
D1mkadon <dima0938157970@gmail.com>
2024-02-22 20:40:27 +0100

    small changes

--- src/app/components/Profile.jsx
+++ src/app/components/Profile.jsx
@@ -1,26 +1,5 @@
-import { useEffect, useState } from "react";
-import useAuth from "../data/useAuth";
-import axios from "axios";
-
 const Profile = () => {
-
-  // const accessToken = useAuth(code);
-  const [profile, setProfile] = useState();
-  // useEffect(() => {
-  //   if (!accessToken) return;
-  //   console.log(accessToken);
-  //   axios
-  //     .get("https://api.spotify.com/v1/me", {
-  //       headers: {
-  //         Authorization: `Bearer ${accessToken}`,
-  //       },
-  //     })
-  //     .then((e) => {
-  //       console.log("user info call", e.data), setProfile(e.data);
-  //     })
-  //     .catch((e) => console.log("user info error", e));
-  // }, [accessToken]);
-  return <div>{code ? <p>{profile.display_name}</p> : null}</div>;
+  return <div>Profile</div>;
 };
 
 export default Profile;
